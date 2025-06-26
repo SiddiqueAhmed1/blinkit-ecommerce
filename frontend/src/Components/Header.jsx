1c13269b176caa5e7c915f41dc31c03c436a2a24
Siddique Ahmed <developersiddique123@gmail.com>
2025-06-26 07:41:58 +0600

    feat: commpleted the header

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -6,26 +6,26 @@ import { FaRegUser } from "react-icons/fa";
 const Header = () => {
   return (
     <>
-      <header className="sticky top-0 w-full shadow">
-        <div className="max-w-[1300px] m-auto flex justify-between items-center p-4">
+      <header className="sticky top-0 w-full shadow pb-3 lg:pb-0">
+        <div className="max-w-[1300px] m-auto flex justify-between items-center p-2">
           <div className="header-left lg:w-[25%]">
             <Link to={"/"} className="inline-block">
               <img
-                className="h-full w-40 object-contain"
+                className="h-full lg:w-40 w-28 sm:w-32 md:w-36 object-contain"
                 src="../public/images/Capture-removebg-preview.png
             "
                 alt=""
               />
             </Link>
           </div>
-          <div className="w-[100%] hidden">
+          <div className="w-full mr-6 hidden lg:block">
             <Search />
           </div>
           <div className="lg:w-[25%]">
-            <button className="lg:hidden">
-              <FaRegUser />
+            <button className="lg:hidden mr-5">
+              <FaRegUser size={25} />
             </button>
-            <div className="header-login  sm:hidden lg:flex justify-end gap-15 hidden">
+            <div className="header-login  hidden lg:flex gap-15">
               <button className="text-xl">
                 <a href="">Login</a>
               </button>
@@ -37,6 +37,9 @@ const Header = () => {
               </button>
             </div>
           </div>
+        </div>
+        <div className="lg:hidden px-4 ">
+          <Search />
         </div>
       </header>
     </>
