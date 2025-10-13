7609c7ced86c88b0724011bbbeed3d4bd239d966
Siddique Ahmed <developersiddique123@gmail.com>
2025-10-13 23:47:07 +0600

    working on category edit

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -36,7 +36,7 @@ const Category = () => {
       {loader ? <Loader /> : ""}
       <section>
         <div className="shadow p-2 rounded flex justify-between items-center ">
-          <h1 className="font-semibold text-2xl">Category</h1>
+          <h1 className="font-semibold text-2xl">Add Category</h1>
           <button
             onClick={() => setOpenCategoryModal(true)}
             className="border border-amber-300 px-4 py-3 rounded cursor-pointer hover:bg-amber-300 hover:text-green-600  font-semibold"
