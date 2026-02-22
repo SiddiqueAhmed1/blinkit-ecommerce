00efb12b5180686a850ee0ca78a0e6d30b157cbb
Siddique Ahmed <developersiddique123@gmail.com>
2026-02-22 23:35:13 +0600

    fix: path problem solved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import browserRouter from "./routing/Index";
+import browserRouter from "./routing";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={browserRouter} />
-  </Provider>
+  </Provider>,
 );
