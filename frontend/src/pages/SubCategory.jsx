ade7a83ae20acdb584468b7b3b2bbbe210ebe4eb
Siddique Ahmed <developersiddique123@gmail.com>
2026-03-07 19:59:07 +0600

    feat: working on sub category

--- frontend/src/pages/SubCategory.jsx
+++ frontend/src/pages/SubCategory.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import SubCategoryUploadModal from "./SubCategoryUploadModal";
+
 const SubCategory = () => {
-  return <div>SubCategory</div>;
+  const [openSubCategoryModal, setOpenSubCategoryModal] = useState(false);
+  // const [subCategoryStore, setSubCategoryStore] = useState([]);
+
+  return (
+    <>
+      <section onClick={() => setOpenSubCategoryModal(false)}>
+        <div className="shadow p-2 rounded flex justify-between items-center gap-3 lg:gap-0">
+          <h1 className="font-semibold text-2xl">Add Sub-Category</h1>
+          <button
+            onClick={() => setOpenSubCategoryModal(true)}
+            className="border border-amber-300 px-4 py-3 rounded cursor-pointer hover:bg-amber-300 hover:text-green-600  font-semibold"
+          >
+            Add Sub-Category
+          </button>
+        </div>
+
+        {openSubCategoryModal && <SubCategoryUploadModal />}
+      </section>
+    </>
+  );
 };
 
 export default SubCategory;
---
+++ frontend/src/pages/SubCategoryUploadModal.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SubCategoryUploadModal = () => {
+  return (
+    <>
+      <section className=" bg-gray-400/60 fixed flex left-0 right-0 top-0 transition justify-center items-center bottom-0 ">
+        <div className="w-[500px] h-[400px] rounded p-4 bg-white  ">
+          <h1>sub category add</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SubCategoryUploadModal;
