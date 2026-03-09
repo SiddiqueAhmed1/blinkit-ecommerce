278c4e73933bb1739ec153b40eedd0db632ca32a
Siddique Ahmed <developersiddique123@gmail.com>
2026-03-09 17:09:13 +0600

    feat: working on sub category upload

--- frontend/src/pages/SubCategoryUploadModal.jsx
+++ frontend/src/pages/SubCategoryUploadModal.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import { MdClose } from "react-icons/md";
 const SubCategoryUploadModal = ({ close }) => {
+  const [data, setData] = useState({
+    subCategoryName: "",
+    image: "",
+  });
   return (
     <>
       <section
@@ -9,7 +15,25 @@ const SubCategoryUploadModal = ({ close }) => {
           onClick={(e) => e.stopPropagation()}
           className="w-[500px] h-[400px] rounded p-4 bg-white  "
         >
-          <h1>sub category add</h1>
+          <div className=" flex justify-between ">
+            <h1 className="text-lg font-semibold">Add Sub Category</h1>
+            <button className="cursor-pointer" onClick={close}>
+              <MdClose size={26} />
+            </button>
+          </div>
+          <div className="py-3">
+            <form action="">
+              <input
+                onChange={(e) => setData(e.target.value)}
+                type="text"
+                name="subCategoryName"
+                value={data.subCategoryName}
+                className="outline-none border border-neutral-300 p-3 rounded"
+                placeholder="type you sub category"
+              />
+              <div>{data.image ? "" : ""}</div>
+            </form>
+          </div>
         </div>
       </section>
     </>
